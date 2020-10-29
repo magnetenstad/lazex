76fa8c62d53002a1d510ba5385562ec780fb53ec
magnetenstad <tenstad.magne@gmail.com>
2020-10-29 19:43:48 +0100

    Add eval and improve functions

--- analyse.py
+++ analyse.py
@@ -1,68 +1,78 @@
-functions = {}
-mode_math = False
 
-def analyse_division(string):
-	parts = string.partition("/")
-	dividend, divisor = parts[0], parts[2]
-	start, end = "", ""
-	for i0 in range(len(parts[0])-1, -1, -1):
-		char = parts[0][i0]
-		if char in "]":
+TEXT = 0
+MATH = 1
+MODE = TEXT
+
+functions = {
+	"eval": ""
+}
+
+def space_string(string):
+	symbols = ("!", "=", "+", "-", "*", "/", ">", "<")
+	i = 0
+	depth = 0
+	while i < len(string):
+		depth += (string[i] in ("(", "{", "[")) - (string[i] in (")", "}", "]"))
+		if depth == 0 and string[i] in symbols:
+			if string[i] != "=" or string[i-1] in symbols:
+				string = string[:i] + " " + string[i:]
+				i += 1
+			if string[i+1] != "=":
+				string = string[:i+1] + " " + string[i+1:]
+		i += 1
+	return string
+
+def analyse_string(string, latex=True):
+	for f in functions:
+		search = "$" + f.split("{")[0]
+		args = f[f.find("{")+1:f.find("}")].replace(" ", "").split(",")
+		while search in string:
+			start = string.find(search)
 			depth = 0
-			b = False
-			for i in range(i0, -1, -1):
-				c = parts[0][i]
-				depth += (c in "[") - (c in "]")
-				if depth == 0 and c in "[":
-					start = parts[0][:i]
-					dividend = parts[0][i:]
-					b = True
+			end = start
+			for i in range(len(string[start:])):
+				char = string[start:][i]
+				depth += (char == "{") - (char == "}")
+				if depth == 0 and char == "}":
+					end = start + i
 					break
-			if b:
-				break
-		elif char in "=+-":
-			start = parts[0][:i0+1]
-			dividend = parts[0][i0+1:]
-			break
-	for i1 in range(len(parts[2])):
-		char = parts[2][i1]
-		if char in "[":
-			depth = 0
-			b = False
-			for i in range(i1, len(parts[2])):
-				c = parts[2][i]
-				depth += (c in "[") - (c in "]")
-				if depth == 0 and c in "]":
-					end = parts[2][i:]
-					divisor = parts[2][:i]
-					b = True
-					break
-			if b:
-				break
-		elif char in "=+-":
-			end = parts[2][i1-1:]
-			divisor = parts[2][:i1-1]
-			break
-	return (start + "\\frac{" + dividend + "}{" + divisor + "}" + end).replace(" ", "")
+			output = functions[f]
+			if f == "eval":
+				argument = string[start+len(search)+1:end]
+				code = analyse_string(argument, latex=False)
+				code = code.replace("[", "(").replace("]", ")")
+				code = code.replace("{", "(").replace("}", ")")
+				code = code.replace("^", "**")
+				print(code)
+				output = str(eval(code))
+			else:
+				arguments = string[start+len(search)+1:end].replace(" ", "").split(",")
+				for i in range(min(len(args), len(arguments))):
+					output = analyse_1(output.replace(args[i], arguments[i]), latex=latex)
+			string = string[:start] + output + string[end+1:]
 
-def analyse_string(string):
-	if "/" in string:
-		string = analyse_division(string)
-	return "[" + string + "]"
+	if latex and MODE == MATH:
+		words = space_string(string).split(" ")
+		while "" in words:
+			words.remove("")
+		while "/" in words:
+			i = words.index("/")
+			dividend, divisor = words.pop(i-1), words.pop(i)
+			words[i-1] = ("\\frac{" + dividend + "}{" + divisor + "}")
+		return "[" + "".join(words) + "]"
+	else:
+		return "[" + string + "]"
 
-def analyse_line(line):
-	global mode_math
-	tabs = line.count("\t")
-	line = line.rstrip("\n").replace("\t", "")
-
-	if mode_math:
+def analyse_1(line, latex=False):
+	global MODE
+	if MODE == MATH:
 		string = "(" + line + ")"
 		i = 0
 		prev = 0
 		while i < len(string):
 			char = string[i]
 			if char == ")":
-				string = string[:prev] + analyse_string(string[prev+1:i]) + string[i+1:]
+				string = string[:prev] + analyse_string(string[prev+1:i], latex=latex) + string[i+1:]
 				i = 0
 				prev = 0
 			elif char == "(":
@@ -72,58 +82,67 @@ def analyse_line(line):
 			string = string[1:-1]
 		
 		line = string
-	command = []
-
-	if line[0] == "\\" and not line[1] == "\\" and not "{" in line:
-		space = line.find(" ")
-		line = line[:space] + "{" + line[space+1:] + "}"
-	if line.startswith("\\begin"):
-		command = ["add", tabs, "\t" * tabs + line.replace("\\begin", "\\end")]
-		if "align" in line or "equation" in line:
-			mode_math = True
-	if line.startswith("\\end"):
-		if "align" in line or "equation" in line:
-			mode_math = False
-	if line.startswith( "\\def"):
-		start = line.find(" ")+1
-		end = line.find("}")+1
-		functions[line[start:end]] = line[end:].strip()
-		print(functions)
-		return "", ["pop"]
-	else:
-		for f in functions:
-			search = "$" + f.split("{")[0]
-			args = f[f.find("{")+1:f.find("}")].replace(" ", "").split(",")
-			while search in line:
-				start = line.find(search)
-				end = start + line[start:].find("}")
-				arguments = line[start+len(search)+1:end].replace(" ", "").split(",")
-				output = functions[f]
-				print(args)
-				print(arguments)
-				for i in range(len(args)):
-					output = output.replace(args[i], arguments[i])
-				line = line[:start] + output + line[end+1:]
 	
-	if mode_math:
-		line = line.replace("[", "\\left(")
-		line = line.replace("]", "\\right)")
-		line = line.replace("sqrt", "\\sqrt")
+	return line
 
-		if line[0] != "\\":
-			line = line.replace("*", "\\cdot")
-			line += "\\\\"
-		
-	return "\t" * tabs + line + "\n", command
+def analyse_line(line, latex=True):
+	global MODE
+	tabs = line.count("\t")
+	line = line.rstrip("\n").replace("\t", "")
+
+	line = analyse_1(line, latex=latex)
+
+	if latex:
+		commands = []
+
+		if line[0] == "\\" and not line[1] == "\\" and not "{" in line:
+			space = line.find(" ")
+			line = line[:space] + "{" + line[space+1:] + "}"
+		if line.startswith("\\begin"):
+			commands.append(["add", tabs, "\t" * tabs + line.replace("\\begin", "\\end")])
+			if "align" in line or "equation" in line:
+				commands.append(["math"])
+		if line.startswith("\\end"):
+			if "align" in line or "equation" in line:
+				MODE = TEXT
+		if line.startswith( "\\def"):
+			start = line.find(" ")+1
+			end = line.find("}")+1
+			functions[line[start:end]] = line[end:].strip()
+			commands.append(["pop"])
+			return "", commands
+
+		if MODE == MATH:
+			line = space_string(line)
+
+			line = line.replace("[", "\\left(")
+			line = line.replace("]", "\\right)")
+			line = line.replace("sqrt", "\\sqrt")
+
+			if line[0] != "\\":
+				line = line.replace("*", "\\cdot ")
+				line += "\\\\"
+			
+	if latex:
+		return "\t" * tabs + line + "\n", commands
+	else:
+		return line, []
 
 def analyse_lines(lines):
+	global MODE
 	i = 0
 	while i < len(lines):
+
+		# Remove empty line
 		if lines[i].isspace():
 			lines.pop(i)
 			continue
-		lines[i], command = analyse_line(lines[i])
-		if len(command):
+		
+		# Analyse line
+		lines[i], commands = analyse_line(lines[i])
+		
+		# Execute command
+		for command in commands:
 			if command[0] == "add":
 				for j in range(i+1, len(lines)):
 					if not lines[j].isspace() and lines[j].count("\t") <= command[1]:
@@ -134,5 +153,10 @@ def analyse_lines(lines):
 			elif command[0] == "pop":
 				lines.pop(i)
 				continue
+			elif command[0] == "math":
+				MODE = MATH
+		
+		# Go to next line
 		i += 1
+
 	return lines
